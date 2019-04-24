0321aacda73a8faa86ff3f0ce68502e6601967e0
antonc27 <antonc27@mail.ru>
2019-04-24 15:38:59 +0200

    Project 4: Mult program

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// In order to calculate R0 * R1, we add R0 in the loop R1 times
+	
+	// Initilization
+	@i
+	M=0 // i=0
+
+	@2
+	M=0 // Init R2=0
+
+(LOOP)
+	// Step check
+	@i
+	D=M
+	@R1
+	D=D-M
+	@END
+	D;JGE // If (i-R1)>=0 goto END
+
+	// Accumulating R0 value in R2
+	@R2
+	D=M
+	@R0
+	D=D+M
+	@2
+	M=D
+
+	// Increment i
+	@i
+	M=M+1 // i=i+1
+	@LOOP
+	0;JMP // Goto LOOP
+(END)
+	@END
+	0;JMP // Infinite loop
